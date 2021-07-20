98a9d65bb9a84b4d5cfe601d8093b638f27ad762
lenovo <lenovo@DESKTOP-6LGJM90>
2021-07-20 14:12:42 +0530

    Branch X

--- Exam_practise/src/Orange.java
+++ Exam_practise/src/Orange.java
@@ -9,6 +9,8 @@ public static void main(String[] args) {
 Orange greenApple = new Orange("green");
 String color = greenApple.getColor();
 System.out.println("color of apple is " + color);
-
+String getColor()
+{
+return color;
 }
 }
